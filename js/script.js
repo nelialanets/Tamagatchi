f508a79fc948b248b37752507e525a1686f31368
Nelia Lanets <78198715+nelialanets@users.noreply.github.com>
2021-11-24 23:24:05 -0800

    started psudo code

---
+++ js/script.js
@@ -0,0 +1,23 @@
+
+// button ckliked start 
+//Alert pops up "hello Name me"
+//Take a name, play btton activates.
+
+
+/*Object 
+Hunder(1-10)
+Sleepines (1-10)
+Boredom (1-10)
+Age(1-10)*/
+
+//is any of the objects reach 0 pet dead
+
+//Buttons:
+//Turn on the light
+//FEEED
+//Play
+//Nemae the pat 
+
+
+
+ 
