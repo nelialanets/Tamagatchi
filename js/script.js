2ff0d622837fe00314390159ebc8ddd07f653f5e
Nelia Lanets <78198715+nelialanets@users.noreply.github.com>
2021-11-26 12:57:10 -0800

    added pseudo code

--- js/script.js
+++ js/script.js
@@ -1,23 +1,28 @@
 
-// button ckliked start 
-//Alert pops up "hello Name me"
-//Take a name, play btton activates.
+/* 1 button "enter "your pet's name" once name is submited the alert button with the pet's name shows up" Hi my name is- .. thank you gor nameing me {name};
+add event listener to the "nameInput" on Submit creat alert button with the pet Name and prompt to click "next".
+1.1 Once next"clicked"  the ".buttonSetup", and ".attributes-section" and "pet" show up fom the left and right ----> "submit button" disapears ----> music plays.  Toggle method?
 
 
-/*Object 
+ prompt to click "Play" btton -- once cliked timer activates alog with ".attributes-section"  and pet's age .*/
+ 
+
+/* ----2 create an object that holds the values on the pet conditions
+Object 
 Hunder(1-10)
 Sleepines (1-10)
 Boredom (1-10)
-Age(1-10)*/
-
+---------*/
 //is any of the objects reach 0 pet dead
 
-//Buttons:
-//Turn on the light
-//FEEED
-//Play
-//Nemae the pat 
+/*----------Buttons:
+Turn on the light on/off addEventListener to dim the light;
+FEEED ------ add addEventListener  and send the message thank you for feeding me"
+//
+---------*/
 
-
+/*-------------------------------
+will add to the EventListener(toggle method) to the right and left side bars 
+for them to show up and disappear.-----*/
 
  
